4d0fea7c9a0b2db4ba624fd4ce39d031de0bc885
Yinn <1499978024@qq.com>
2022-03-31 23:12:15 +0200

    01 02 03 04

--- todo-ninja/src/views/Home.vue
+++ todo-ninja/src/views/Home.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
+    <p class="red white--text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt eligendi quibusdam ex, at ducimus perferendis mollitia vel rem nesciunt unde deserunt non voluptatibus nostrum illum neque placeat consequuntur porro. Tempora.</p>
+    <p class="pink red--text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi sequi voluptatum vel ut iure, optio at iusto! Nobis ipsa, aperiam qui voluptas praesentium sapiente veritatis fuga omnis adipisci cumque itaque?</p>
+    <p class="pink lighten-4 red--text text--darken-4">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi sequi voluptatum vel ut iure, optio at iusto! Nobis ipsa, aperiam qui voluptas praesentium sapiente veritatis fuga omnis adipisci cumque itaque?</p>
+    <h1 class="display-4">Massive Display</h1>
+    <h4 class="display-1">Smaller display</h4>
+    <p class="headline">This is a headline</p>
+    <p class="subheading font-weight-bold">This is a subheading</p>
+    <p class="caption">This is a caption</p>
   </div>
 </template>
 
