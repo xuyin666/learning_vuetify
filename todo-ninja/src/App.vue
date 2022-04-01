1e832bbb374c428c6cca6ba6bccf859df73f56ed
Yinn <1499978024@qq.com>
2022-04-01 15:16:53 +0200

    07

--- todo-ninja/src/App.vue
+++ todo-ninja/src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <v-app>
+  <v-app class="grey lighten-4">
+    <Navbar />
     <v-main>
       <router-view/>
     </v-main>
@@ -7,10 +8,11 @@
 </template>
 
 <script>
+import Navbar from "@/components/Navbar"
 
 export default {
   name: 'App',
-
+  components: {Navbar},
   data: () => ({
     //
   }),
--- todo-ninja/src/components/Navbar.vue
+++ todo-ninja/src/components/Navbar.vue
@@ -1,5 +1,20 @@
 <template>
-  
+  <nav>
+      <v-toolbar flat app>
+          <v-toolbar-title class="text-uppercase grey--text">
+              <span class="font-weight-light">Todo</span>
+              <span>Ninja</span>
+          </v-toolbar-title>
+          <!-- it will grow into all of the available room inside the navbar-->
+          <v-spacer></v-spacer>
+          <!-- when we use the props, the color controls the foreground text color 
+          and the hover color -->
+          <v-btn text color="grey">
+              <span>Sign Out</span>
+              <v-icon right>mdi-exit-to-app</v-icon>
+          </v-btn>
+      </v-toolbar>
+  </nav>
 </template>
 
 <script>
